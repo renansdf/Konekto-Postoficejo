3cfe89d7cfc2b601b19a58d79c08ae277d56921b
renansdf <rnn.frts@gmail.com>
2021-03-26 00:23:31 -0300

    added dotenv support

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config';
 
 import quoteMailerRouter from './routes/quote/QuoteMailer';
 import attachmentsRouter from './routes/attachments/AttachmentsRouter';
