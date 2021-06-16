700dd573bcd4f6fb579c6364f130c7ffb2d9e2de
Renan de Freitas <rnn.frts@gmail.com>
2021-06-16 17:16:42 -0300

    created and called new app error class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
 import quoteMailerRouter from './routes/quote/QuoteMailer';
 import attachmentsRouter from './routes/attachments/AttachmentsRouter';
+import contactRouter from './routes/contact/contactRouter';
+import AppError from './shared/AppError';
 
 const app = express();
 
@@ -12,6 +14,23 @@ app.use(express.json());
 
 app.use('/quote', quoteMailerRouter);
 app.use('/attachments', attachmentsRouter);
+app.use('/contact', contactRouter);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('server started @ 3333');
