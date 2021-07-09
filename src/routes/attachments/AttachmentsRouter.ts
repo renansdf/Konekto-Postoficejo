a8f398a2dd13e4da8f1c71d829de64bb8c6ccd22
Renan de Freitas <rnn.frts@gmail.com>
2021-07-09 17:38:52 -0300

    added error handling for non existent file

--- src/routes/attachments/AttachmentsRouter.ts
+++ src/routes/attachments/AttachmentsRouter.ts
@@ -1,8 +1,9 @@
 import { Router, Request, Response } from 'express';
-import multer from "multer";
+import multer from 'multer';
 
-import S3StorageProvider from '../../providers/StorageProvider/S3StorageProvider';
+import AppError from '../../shared/AppError';
 import configs from '../../configs/storage';
+import S3StorageProvider from '../../providers/StorageProvider/S3StorageProvider';
 
 const attachmentsRouter = Router();
 const upload = multer({ storage: configs.storage });
@@ -10,7 +11,11 @@ const upload = multer({ storage: configs.storage });
 attachmentsRouter.post('/', upload.single('attachment'), async (request: Request, response: Response) => {
   const attachmentFile = request.file;
 
-  const storageProvider = new S3StorageProvider;
+  if (!attachmentFile) {
+    throw new AppError('file not found', 404);
+  }
+
+  const storageProvider = new S3StorageProvider();
   const fileURL = await storageProvider.saveFile(attachmentFile.filename);
 
   return response.json({ fileURL });
