8bb20017007a88804228239f8330bc427149f0cf
Renan de Freitas <rnn.frts@gmail.com>
2021-07-22 16:44:06 -0300

    added test route and updated quote route names

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,16 +3,18 @@ import cors from 'cors';
 
 import AppError from '../shared/AppError';
 
-import quoteMailerRouter from './quote/QuoteMailer';
+import quoteRouter from './quote/QuoteRouter';
 import attachmentsRouter from './attachments/AttachmentsRouter';
 import contactRouter from './contact/contactRouter';
+import testRouter from './test/TestRouter';
 
 const routes = Router();
 routes.use(cors());
 
-routes.use('/quote', quoteMailerRouter);
+routes.use('/quote', quoteRouter);
 routes.use('/attachments', attachmentsRouter);
 routes.use('/contact', contactRouter);
+routes.use('/test', testRouter);
 
 routes.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
