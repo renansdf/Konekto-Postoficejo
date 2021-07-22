cce173f193bb746efab186452761f672d247eb5d
Renan de Freitas <rnn.frts@gmail.com>
2021-07-22 16:41:43 -0300

    changed default port for development

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3333, () => {
-  console.log('server started @ 3333');
+app.listen(process.env.PORT || 3550, () => {
+  console.log('server started');
 });
